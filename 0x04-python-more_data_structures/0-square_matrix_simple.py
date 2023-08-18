993f88bf8346556f1c00081d828df43f4f837903
kimoAth <kimo.daniel11@gmail.com>
2023-08-18 22:50:41 +0100

    matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,12 @@
+#!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
+    """
+    Return the sum of all elements in a matrix
+    """
+
     new_matrix = []
     for row in matrix:
         new_row = [x ** 2 for x in row]
         new_matrix.append(new_row)
     return new_matrix
+
